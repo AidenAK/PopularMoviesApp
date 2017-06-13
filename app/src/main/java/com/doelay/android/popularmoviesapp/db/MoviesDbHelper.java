f29247583276628ba8153c604e87532c1342d5b5
Doelay <nithedoe@gmail.com>
2017-06-13 14:50:54 -0400

    Added MoviesDbHelper class

---
+++ app/src/main/java/com/doelay/android/popularmoviesapp/db/MoviesDbHelper.java
@@ -0,0 +1,34 @@
+package com.doelay.android.popularmoviesapp.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by doelay on 6/13/2017.
+ */
+
+public class MoviesDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "moviesDb.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MoviesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String CREATE_TABLE = "CREATE TABLE " + MoivesContract.MoviesEntry.TABEL_NAME + " (" +
+                MoivesContract.MoviesEntry.MOVIE_ID + " INTEGER PRIMARY KEY, " +
+                MoivesContract.MoviesEntry.MOVIE_TITLE + " TEXT NOT NULL);";
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MoivesContract.MoviesEntry.TABEL_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
