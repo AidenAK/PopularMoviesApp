75129af2b73868d4c0bffacaf8711ba0a5b8124f
Doelay <nithedoe@gmail.com>
2017-06-20 22:16:26 -0400

    Added back functionality to FavoriteMovieActivity.

--- app/src/main/java/com/doelay/android/popularmoviesapp/activity/FavoriteMovieActivity.java
+++ app/src/main/java/com/doelay/android/popularmoviesapp/activity/FavoriteMovieActivity.java
@@ -3,6 +3,7 @@ package com.doelay.android.popularmoviesapp.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.doelay.android.popularmoviesapp.R;
 
@@ -17,7 +18,18 @@ public class FavoriteMovieActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // TODO: 6/20/2017 load favorite movies from content provider
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
