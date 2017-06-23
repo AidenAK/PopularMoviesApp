ff394fce03b787d79ee954f31248bc4dfc0546de
Doelay <nithedoe@gmail.com>
2017-06-22 22:18:37 -0400

    Review object is parcelable now

--- app/src/main/java/com/doelay/android/popularmoviesapp/model/Review.java
+++ app/src/main/java/com/doelay/android/popularmoviesapp/model/Review.java
@@ -1,10 +1,13 @@
 package com.doelay.android.popularmoviesapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by doelay on 6/19/2017.
  */
 
-public class Review {
+public class Review implements Parcelable{
 
     private String authorName;
     private String review;
@@ -13,6 +16,37 @@ public class Review {
         this.authorName = authorName;
         this.review = review;
     }
+
+    private Review(Parcel in) {
+        authorName = in.readString();
+        review = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int i) {
+        out.writeString(authorName);
+        out.writeString(review);
+    }
+
+    public static final Parcelable.Creator<Review> CREATOR
+            = new Parcelable.Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel parcel) {
+            return new Review(parcel);
+        }
+
+        @Override
+        public Review[] newArray(int i) {
+            return new Review[0];
+        }
+    };
+
+
 
     public String getAuthorName() {
         return authorName;
